ee14f07940e328603a0b419f832b32b46fae4d8e
oleh.semeniuk <oleh.semeniuk@contractor.p66.com>
2023-02-20 18:25:56 +0100

    Add enemies generator on map

---
+++ traveler/src/components/EnemyTile.vue
@@ -0,0 +1,43 @@
+<template>
+    <button class="enemyTile" v-if="enemyShown" @click="attackEnemy(tile)"></button>
+</template>
+
+<script lang="ts">
+import TileModel from '@/models/TileModel';
+export default {
+    name: "enemy-tile",
+    props: {
+        tile: {
+            type: TileModel,
+            required: true
+        }
+    },
+    data() {
+            let enemyShown = true;
+            return { enemyShown }
+        },
+    methods: {
+        async attackEnemy(tile: TileModel) {
+            const el = document.getElementById("" + tile.id);
+            console.log(el);
+            if(el) {
+                this.enemyShown = false;
+            }
+        }
+    }
+}
+</script>
+<style>
+.enemyTile {
+    width: 100px;
+    height: 100px;
+    background: none;
+    border: 2px solid rgb(255, 115, 0);
+    border-radius: 50%;
+    margin-left: 2%;
+    display: inline-flex;
+    align-items: flex-end;
+    background-image: url('../assets/images/enemies/twoEnemies.png');
+    background-size: 100% 100%;
+}
+</style>
---
+++ traveler/src/components/GameMap.vue
@@ -0,0 +1,35 @@
+<template>
+     <div class="map">
+        <map-tile v-for="mapTile in tiles" :key="mapTile.id" :tile="mapTile"></map-tile>
+    </div>
+    <button @click="quitDungeon()">Quit</button>
+</template>
+
+<script lang="ts">
+import router from '@/router';
+import TileModel from '@/models/TileModel'
+import MapTile from './MapTile.vue';
+
+export default {
+    name: "GameMap",
+    components: { MapTile },
+    props:  {
+        tiles: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        async quitDungeon() {
+            router.push("/");
+        },
+        async clearTile(tile: TileModel) {
+            const el = document.getElementById("" + tile.id);
+            console.log(el);
+            if(el) {
+                el.style.visibility = 'hidden';
+            }
+        }
+    }
+}
+</script>
