d3d51151b68cb7f127e885fb763b421490bcbfac
semeniuk.roman <rcemeniuk@gmail.com>
2023-03-19 16:59:47 +0100

    fix increase current hero health by equipment use

--- traveler/src/components/BagItemTile.vue
+++ traveler/src/components/BagItemTile.vue
@@ -50,18 +50,15 @@ export default {
                 if(this.hero.equipment.armor != null) {
                     this.bagStore.removeItem(item);
                     this.hero.maxHealth -= this.hero.equipment.armor.value;
-                    this.hero.currentHealth -= this.hero.equipment.armor.value;
                     this.bagStore.putIn(this.hero.equipment.armor);
                     this.hero.equipment.armor = item;
                     this.hero.maxHealth += this.hero.equipment.armor.value;
-                    this.hero.currentHealth += this.hero.equipment.armor.value;
                     
                     return true;
                 } else if (this.hero.equipment.armor == null) {
                     this.bagStore.removeItem(item);
                     this.hero.equipment.armor = item;
                     this.hero.maxHealth += this.hero.equipment.armor.value;
-                    this.hero.currentHealth += this.hero.equipment.armor.value;
                     return true;
                 }
             } else if( item.itemType === ItemType.HEAL ) {
@@ -75,18 +72,15 @@ export default {
                 if(this.hero.equipment.helm != null) {
                     this.bagStore.removeItem(item);
                     this.hero.maxHealth -= this.hero.equipment.helm.value;
-                    this.hero.currentHealth -= this.hero.equipment.helm.value;
                     this.bagStore.putIn(this.hero.equipment.helm);
                     this.hero.equipment.helm = item;
                     this.hero.maxHealth += this.hero.equipment.helm.value;
-                    this.hero.currentHealth += this.hero.equipment.helm.value;
                     
                     return true;
                 } else if (this.hero.equipment.helm == null) {
                     this.bagStore.removeItem(item);
                     this.hero.equipment.helm = item;
                     this.hero.maxHealth += this.hero.equipment.helm.value;
-                    this.hero.currentHealth += this.hero.equipment.helm.value;
                     return true;
                 }
             }
