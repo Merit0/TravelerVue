27fc4f64896c0a4ba2a4d040a2a48f4ef3f6da45
Oleh Semeniuk <oleh.semeniuk@avenga.dev>
2025-03-29 22:02:38 +0100

    update maps view page

---
+++ traveler/src/components/dungeon-tile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dungeonTile dungeon-point pulsing" v-if="!isEmpty">
+    <button @click="startDungeon()" class="buttonMap" :disabled="mapStore.isMapCleared"></button>
+    <div class="resetBtnBackground" v-if="mapStore.isMapCleared">
+      <div class="resetImg">
+        <button class="btnReset" @click="mapStore.resetMap"></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import router from '@/router/index';
+import {useMapStore} from '@/stores/MapStore';
+
+export default {
+  name: "dungeon-tile",
+  props: {
+    isEmpty: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    const mapStore = useMapStore();
+    let isDisabled = false;
+    return {isDisabled, mapStore};
+  },
+  methods: {
+    async startDungeon() {
+      router.push("/old-forest");
+    }
+  }
+}
+</script>
+
+<style>
+@import url('@/styles/dungeon-spot.css');
+</style>
