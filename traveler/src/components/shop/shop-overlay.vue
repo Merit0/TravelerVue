86ba31b0bf6045e461100bc0debf19bbc8a03ebe
oleh.semeniuk <oleh.semeniuk@avenga.com>
2025-04-30 11:09:14 +0200

    remove all unnessesary components from shop overlay

--- traveler/src/components/shop/shop-overlay.vue
+++ traveler/src/components/shop/shop-overlay.vue
@@ -10,11 +10,9 @@
 </template>
 
 <script lang="ts">
-import {useHeroStore} from "@/stores/HeroStore";
 import {useShopStore} from "@/stores/shop-store";
 import {LootItemModel} from "@/models/LootItemModel";
 import ShopSlotsGrid from "@/components/shop/shop-slots-grid.vue";
-import {useBagStore} from "@/stores/BagStore";
 import ShopModeSwitcher from "@/components/shop/shop-mode-switcher.vue";
 import TradeSlotsGrid from "@/components/shop/trade-slots-grid.vue";
 
@@ -30,47 +28,19 @@ export default {
     }
   },
   data() {
-    const heroStore = useHeroStore();
     const shopStore = useShopStore();
-    const bagStore = useBagStore();
     shopStore.initShopItems();
     const itemsList: LootItemModel[] = shopStore.getShopItems();
     let isBuyMode = true;
-    const hoveredItem: LootItemModel = null;
-    let itemToSell: LootItemModel = null;
-    let showConfirmModal = false;
     return {
       itemsList,
-      heroStore,
       isBuyMode,
-      bagStore,
-      hoveredItem,
-      showConfirmModal,
-      itemToSell
     };
   },
   methods: {
     async closeShop() {
       this.showShop = false;
     },
-    // confirmSell(item: LootItemModel) {
-    //   this.itemToSell = item;
-    //   console.log(this.itemToSell);
-    //   this.showConfirmModal = true;
-    // },
-    // sellItem() {
-    //   if (this.itemToSell) {
-    //     console.log(`Sold ${this.itemToSell.name} for ${this.itemToSell.price} coins!`);
-    //     this.heroStore.collect(this.itemToSell.price);
-    //     this.bagStore.removeItem(this.itemToSell.id);
-    //     this.itemToSell = null;
-    //   }
-    //   this.showConfirmModal = false;
-    // },
-    // cancelSell() {
-    //   this.itemToSell = null;
-    //   this.showConfirmModal = false;
-    // }
   },
 };
 </script>
